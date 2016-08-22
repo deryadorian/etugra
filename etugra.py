92d801b4bc01ee9495e5bb286be6fa95f5288583
funkydorian <derya.sezen@gmail.com>
2016-08-22 18:31:14 +0300

    comments organized

--- etugra.py
+++ etugra.py
@@ -5,10 +5,7 @@ import sys
 import getopt
 from zeep import Client
 
-#Step1: input dosya SHA256
-
-    #Step2: etugra service client definition
-
+#etugra service client definitions
 WSDL = 'http://edocservice.e-tugra.com.tr/services/TsaService.asmx?wsdl'
 SOAPClient = Client(wsdl=WSDL)
 
@@ -16,6 +13,8 @@ try:
     opts, args = getopt.getopt(sys.argv[1:],"tg:v:",["inputlogfile==","verifytimestamp"])
 except getopt.GetoptError:
     print("etugra.py -t")
+    print("etugra.py -g <file>")
+    print("etugra.py -v <file> <timestamp>")
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-t':  #Check the current number of tokens left       
@@ -23,8 +22,8 @@ for opt, arg in opts:
     elif opt  == '-g': #gettimestamp for a file
         inputfile = sys.argv[2]
         filecontent = open(inputfile,'r').read().encode('utf-8')
-        sha = hashlib.sha256()
-        sha.update(filecontent)
+        sha = hashlib.sha256() 
+        sha.update(filecontent) #SHA256 of the input file
         timestamp = SOAPClient.service.GetTimeStamp(sha.digest(),'test','test')
         print(timestamp)
 #Step3: input dizin ve etugra response file concat
