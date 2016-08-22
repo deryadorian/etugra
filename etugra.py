2529b6c84807fd005343b0142e84f88237f09618
funkydorian <derya.sezen@gmail.com>
2016-08-22 18:33:07 +0300

    help organized

--- etugra.py
+++ etugra.py
@@ -12,9 +12,9 @@ SOAPClient = Client(wsdl=WSDL)
 try:
     opts, args = getopt.getopt(sys.argv[1:],"tg:v:",["inputlogfile==","verifytimestamp"])
 except getopt.GetoptError:
-    print("etugra.py -t")
-    print("etugra.py -g <file>")
-    print("etugra.py -v <file> <timestamp>")
+    print("etugra.py -t                         Show the number of tokens left")
+    print("etugra.py -g <file>                  Get the timestamp token of the <file>")
+    print("etugra.py -v <file> <timestamp>      Verify the timestamp of the file")
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-t':  #Check the current number of tokens left       
